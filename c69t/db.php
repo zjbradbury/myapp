ef56136a2cf3721cef0543faf2dc5c34ed7da536
zjbradbury <88970824+zjbradbury@users.noreply.github.com>
2026-04-02 14:59:51 +1030

    db example page

---
+++ c69t/db.php
@@ -0,0 +1,19 @@
+        <?php 
+        $conn=mysqli_connect("mariadb", "zack", "BradburyLeilani1", "table");
+
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to database: <br>".mysqli_connect_error();
+    }
+
+    else {}
+
+    $query="SELECT * FROM `table`";
+    $result=mysqli_query($conn, $query);
+    // $row = mysqli_fetch_array( $result );
+
+    
+    while($row=mysqli_fetch_array($result)) {
+        echo '<h1>' .$row['row']. '</h1>';
+}
+
+    ?>
