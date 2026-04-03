1245430abf5948ebac6437e0bfbdb9879715c392
zjbradbury <88970824+zjbradbury@users.noreply.github.com>
2026-04-04 08:39:45 +1030

    Update sample.php

--- c69t/sample.php
+++ c69t/sample.php
@@ -56,8 +56,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sample Entry</title>
-
-    <link rel="shortcut icon" href="favicon.ico">
     <link rel="stylesheet" href="style.css">
 </head>
 
