290cbf2230726d2a7dfa5908423389f22ee3eea9
zjbradbury <88970824+zjbradbury@users.noreply.github.com>
2026-04-04 08:38:20 +1030

    Update gas_test.php

--- c69t/gas_test.php
+++ c69t/gas_test.php
@@ -61,8 +61,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gas Test Entry</title>
-
-    <link rel="shortcut icon" href="favicon.ico">
     <link rel="stylesheet" href="style.css">
 </head>
 
